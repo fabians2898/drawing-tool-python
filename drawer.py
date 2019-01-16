959d47bc3359d11a23ef0ef30ac1e2dbaf65b05e
fabians2898 <fabians2898@gmail.com>
2019-01-15 23:05:43 -0500

    Finish paint_line() and add borders to canvas

--- drawer.py
+++ drawer.py
@@ -1,32 +1,37 @@
 # drawer.py
 import click
-import re
 
 
 class Drawer:
 
     def __init__(self):
         self.only_canvas = []
-        self.regexs = {
-            'canvas': r'C\s\d+\s\d+',
-            'line': r'L\s\d+\s\d+\s\d+\s\d+'
-        }
 
-    def paint_empty_canvas(self, height, width):
-        print('height: {}, width: {}'.format(height, width))
-        self.only_canvas = [[0 for x in range(width)] for y in range(height)]
+    def create_canvas(self, height, width):
+        self.only_canvas = [['' for x in range(height)] for y in range(width)]
+        for index,row in enumerate(self.only_canvas):
+            if index == 0 or index == len(self.only_canvas)-1:
+                self.only_canvas[index] = ['#' for x in self.only_canvas[index]]
+            row[0], row[-1] = '#', '#'
 
     def paint_line(self, x1, x2, y1, y2):
         if len(self.only_canvas) > 0:
-            if x1 != y1 or x2 != y2:
-                print('coordinate position is not for a straight line')
+            if x1 != y1 and x2 != y2:
+                print('positions doesn\'t work for a straight line')
+                exit()
 
-            if x1 == y1:
-                diff = x2 - y2
-            elif x2 == y2:
-                diff = x1 - y1
-            self.only_canvas[x1][x2] = 'L'
-            self.only_canvas[y1][y2] = 'L'
+            try:
+                self.only_canvas[x2][x1] = 'L'
+            except Exception as err:
+                print('Line cannot be painted')
+                exit()
+
+            try:
+                self.only_canvas[y2][y1] = 'L'
+            except Exception as err:
+                print('Line cannot be painted')
+                exit()
+
         else:
             print('There is no canvas')
             exit()
@@ -36,6 +41,9 @@ class Drawer:
         f.write(new_line + '\n')
         f.close()
 
+    def print_canvas(canvas):
+        print([''.join(map(str, x)) for x in canvas])
+
 @click.command()
 @click.option('--file', '-f')
 def main(file):
@@ -44,14 +52,16 @@ def main(file):
     lines = r_file.readlines()
     for line in lines:
         if line[0] == 'C':
-            p = [int(s) for s in line.split() if s.isdigit()]
-            drawer.paint_empty_canvas(p[0], p[1])
-            print(drawer.only_canvas)
+            p = [int(s)+2 for s in line.split() if s.isdigit()]
+            drawer.create_canvas(p[0], p[1])
+            for row in drawer.only_canvas:
+                print(row)
         elif line[0] == 'L':
             x1, x2, y1, y2 = [int(s) for s in line.split() if s.isdigit()]
+            print(x1, x2, y1, y2)
             drawer.paint_line(x1, x2, y1, y2)
-            print(drawer.only_canvas)
-
+            for row in drawer.only_canvas:
+                print(row)
 
 if __name__ == "__main__":
     main()
